ea82774e9aeca79dd32bcfb4bf13ca94d9348dbc
Scott Stringer <scott_ts@hotmail.com>
2021-05-03 16:41:59 -0500

    finished exercise 2

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyBestPractices
+{
+    class DapperProductRepository : IProductRepository
+    {
+        //Field or local Variable for making queries to the DB
+        private readonly IDbConnection _connection;
+
+        //Constructor
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var products = _connection.Query<Product>("SELECT * FROM products").ToList();
+            return products;
+        }
+
+        public void CreateProduct(string newName, double newPrice, int newCategoryID)
+        {
+            _connection.Execute("INSERT INTO PRODUCTS (Name, Price, CategoryID) VALUES (@name, @price, @categoryId);",
+            new { name = newName, price = newPrice, categoryId = newCategoryID });
+        }
+    }
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -9,6 +9,21 @@ namespace BestBuyBestPractices
 {
     class Program
     {
+        private static void PrintDepartment(IEnumerable<Department> depos)
+        {
+            foreach (var depo in depos)
+            {
+                Console.WriteLine($"Id: {depo.DepartmentID} Name: {depo.Name}");
+            }
+        }
+        private static void PrintProduct(IEnumerable<Product> products)
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Id: {product.ProductID} Name: {product.Name} Price: {product.Price} CatagoryID: {product.CategoryID} On Sale: {product.OnSale} Stock Level: {product.StockLevel}");
+            }
+        }
+
         static void Main(string[] args)
         {
             #region Configuration
@@ -21,14 +36,15 @@ namespace BestBuyBestPractices
             #endregion
 
             IDbConnection conn = new MySqlConnection(connString);
-            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+            DapperDepartmentRepository depRepo = new DapperDepartmentRepository(conn);
+            DapperProductRepository prodRepo = new DapperProductRepository(conn);
 
-            Console.WriteLine("Hello user, her are the current departments:");
+            Console.WriteLine("Hello user, here are the current departments:");
             Console.WriteLine("Please press enter . . .");
             Console.ReadLine();
 
-            var depos = repo.GetAllDepartments();
-            Print(depos);
+            var depos = depRepo.GetAllDepartments();
+            PrintDepartment(depos);
 
             Console.WriteLine("Do you want to add a department?");
             string userResponse = Console.ReadLine();
@@ -37,19 +53,34 @@ namespace BestBuyBestPractices
             {
                 Console.WriteLine("What is the name of your new Department?");
                 userResponse = Console.ReadLine();
-                repo.InsertDepartment(userResponse);
-                Print(repo.GetAllDepartments());
+                depRepo.InsertDepartment(userResponse);
+                PrintDepartment(depRepo.GetAllDepartments());
             }
 
-            Console.WriteLine("Have a great day!");
-        }
+            Console.WriteLine();
+            Console.WriteLine("Now here are the current products:");
+            Console.WriteLine("Please press enter . . .");
+            Console.ReadLine();
 
-        private static void Print(IEnumerable<Department> depos)
-        {
-            foreach (var depo in depos)
+            var products = prodRepo.GetAllProducts();
+            PrintProduct(products);
+
+            Console.WriteLine("Do you want to add a product?");
+            string userResponse2 = Console.ReadLine();
+
+            if (userResponse2.ToLower() == "yes")
             {
-                Console.WriteLine($"Id: {depo.DepartmentID} Name: {depo.Name}");
+                Console.WriteLine("What is the name of your new Product?");
+                string userResponse3 = Console.ReadLine();
+                Console.WriteLine("What is the price of your new Product?");
+                string userResponse4 = Console.ReadLine();
+                Console.WriteLine("What is the category ID of your new Product?");
+                string userResponse5 = Console.ReadLine();
+                prodRepo.CreateProduct(userResponse3, Double.Parse(userResponse4), Int32.Parse(userResponse5));
+                PrintProduct(prodRepo.GetAllProducts());
             }
         }
+
+       
     }
 }
